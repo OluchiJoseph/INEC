205157e9f2e8509edbcc553da1d2f3fe359d95a3
support <support@WEMA-TRAINING-S>
2022-08-10 10:52:18 -0700

    Add project files.

---
+++ INEC/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+
+namespace INEC
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] votersName = { "John", "Ada", "Chika", "Ayo", "Ojo", "Ada", "Chika", "Ayo", "Adamu", "Bola" };
+
+            Dictionary<string, int> userDic = new Dictionary<string, int>();
+            foreach (var item in votersName)
+            {
+                if (userDic.ContainsKey(item))
+                {
+                    userDic[item]++;
+                }
+                else
+                {
+                    userDic[item] = 1;
+                }
+
+            }
+            Console.WriteLine("Names of users and number of times registered");
+
+            foreach (var item in userDic)
+            {
+                if (item.Value > 1)
+                {
+                    Console.WriteLine(item.Key + ": " + item.Value);
+                }
+            }
+
+            // Using hashset to detect duplicate names 
+
+            Console.WriteLine("\n\nUnique voters list");
+
+            HashSet<string> unique = votersName.ToHashSet();
+            foreach (string item in unique)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
